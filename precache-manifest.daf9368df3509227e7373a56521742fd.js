634921a5f8980d8144f59957641ef5c94db864f4
Josh <josh.asi@hotmail.co.nz>
2019-11-01 13:59:14 +1300

    Updates

---
+++ precache-manifest.daf9368df3509227e7373a56521742fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5827896ddf58015b8674cd20bc84abb",
+    "url": "/sarcasm/index.html"
+  },
+  {
+    "revision": "ee411950699f608755b8",
+    "url": "/sarcasm/static/css/main.b2642d98.chunk.css"
+  },
+  {
+    "revision": "5536271cd0168b48619e",
+    "url": "/sarcasm/static/js/2.cc7dd9f9.chunk.js"
+  },
+  {
+    "revision": "ee411950699f608755b8",
+    "url": "/sarcasm/static/js/main.16af6e1d.chunk.js"
+  },
+  {
+    "revision": "217198ab71ef44066cd5",
+    "url": "/sarcasm/static/js/runtime-main.fbee813b.js"
+  }
+]);
